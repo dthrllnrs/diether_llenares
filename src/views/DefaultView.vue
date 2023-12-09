f32a98aecebbb849690d366305fb38662a6e3570
Diether <llenaresdiether@gmail.com>
2023-12-09 15:21:29 +0800

    Initial commit

---
+++ src/views/DefaultView.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <div class="container">
+      <p class="text-primary">Hello World!</p>
+    </div>
+  </main>
+</template>
+<script>
+// import { RouterView } from 'vue-router'
+export default {
+
+}
+</script>
