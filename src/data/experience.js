262ae70280d2d4a3d6a68b01237361b1c47b06c7
Diether <diether@chykalophia.com>
2025-04-11 04:19:20 +0800

    update 2025

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,73 +1,107 @@
 export default ([
-    {
-        title: 'Front End Developer',
-        company: 'Kinetic Innovative Staffing',
-        company_website: 'https://www.kineticstaff.com/',
-        duration: {
-            from: 'March 2023',
-            to: 'Present'
-        },
-        description: 'Managed development and maintenance of multiple Vue.js web portal projects, showcasing expertise in project management and technical execution. Successfully led the complete lifecycle of the Read Aloud Challenge Portal, from concept to deployment. Implemented Vue.js functionality and design with precision, aligning closely with Figma specifications for a user-centric experience.',
-        stack: ['VueJS', 'Bootstrap', 'Pinia', 'SCSS'],
-        projects: [
-            {
-                title: 'Read Aloud Challenge Portal',
-                link: 'https://www.readaloudchallenge.co.uk/',
-            },
-        ]
+  {
+    title: 'Web Development Team Lead',
+    company: 'Chykalophia Group',
+    company_website: null, // Or add if available
+    duration: {
+      from: 'May 2024',
+      to: 'Present'
     },
-    {
-        title: 'Full Stack Developer',
-        company: 'Pictureworks Group Pty. Ltd.',
-        company_website: 'https://www.pictureworks.com.au/',
-        duration: {
-            from: 'March 2022',
-            to: 'Feb 2023'
-        },
-        description: 'Joined a SaaS development team focused on managing printing businesses, contributing significantly to portal enhancement and maintenance. Utilized ReactJS for feature implementation and optimization of queries handling large datasets, enhancing platform performance. Proficiently translated design concepts into functional front-end code, demonstrating effective communication between design and execution in ReactJS.',
-        stack: ['ReactJS', 'Bootstrap', 'Laravel', 'PostgreSQL'],
+    description: 'Promoted to Team Lead, spearheading the company\'s transition to Agile methodology for improved efficiency and faster project delivery. Proven expertise in the TALL stack (Tailwind CSS, Alpine.js, Laravel, Livewire) to drive streamlined development processes. Leading a collaborative team environment to deliver high-quality projects on time.',
+    stack: ['Laravel', 'Livewire', 'Alpine.js', 'Tailwind CSS'],
+    projects: [
+      {
+        title: 'CareDocs',
+        link: '#',
+      },
+    ]
+  },
+  {
+    title: 'Full-Stack Web Developer',
+    company: 'Chykalophia Group',
+    company_website: null, // Or add if available
+    duration: {
+      from: 'February 2024',
+      to: 'May 2024'
     },
-    {
-        title: 'Full Stack Developer',
-        company: 'Cargo Padala Express',
-        company_website: 'https://capex.com.ph/',
-        duration: {
-            from: 'Feb 2021',
-            to: 'March 2022'
-        },
-        description: 'Led the overhaul of Capex Booking Website from WordPress to Laravel, managing the project lifecycle. Implemented Laravel for backend, Laravel Blade for frontend, and MySql for efficient data handling. Integrated Google Maps API for enhanced package tracking. Also, spearheaded the development of a competitive TNVS application in the Philippines.',
-        stack: ['VueJS', 'Bootstrap', 'Laravel', 'MySQL'],
-        projects: [
-            {
-                title: 'Capex Booking',
-                link: 'https://app.capex.com.ph/',
-            },
-            {
-                title: 'BluePanther',
-                link: 'https://tnvs.capex.com.ph/'
-            }
-        ]
+    description: 'Assumed responsibility for development and maintenance of multiple client projects. Demonstrated versatility by utilizing the TALL stack for development and leveraging CMS platforms like WordPress and Shopify for client projects, adapting to diverse project needs. Optimized database queries for improved query execution times, ultimately enhancing application responsiveness. Collaborated with cross-functional teams to ensure seamless integration of front-end and back-end components, delivering fully functional web applications.',
+    stack: ['Laravel', 'Livewire', 'Alpine.js', 'Tailwind CSS', 'WordPress', 'Shopify'],
+    projects: [
+      {
+        title: 'Telling Industries - Product Finder',
+        link: 'https://tellingindustries.com/product-finder-telling/',
+      },
+    ]
+  },
+  {
+    title: 'Front End Developer',
+    company: 'Kinetic Innovative Staffing',
+    company_website: 'https://www.kineticstaff.com/',
+    duration: {
+      from: 'March 2023',
+      to: 'Present'
     },
-    {
-        title: 'Full Stack Developer',
-        company: 'Tailin Abrasives Corporation',
-        company_website: 'https://www.tailinsubic.net/',
-        duration: {
-            from: 'Feb 2020',
-            to: 'August 2020'
-        },
-        description: `Joined Tailin as one of two inaugural developers, pivotal in forming the company's IT team. Played a key role in designing, developing, and maintaining the ERP system, overseeing functions like production management, reporting, attendance tracking, and internal communications. Specialized in backend development with NodeJS, successfully implementing advanced features, including real-time chat and live production tracking. Also contributed to translating design concepts into frontend code using Angular 8.`,
-        stack: ['Angular 8', 'MaterialCSS', 'Laravel', 'NodeJS'],
+    description: 'Managed development and maintenance of multiple Vue.js web portal projects, showcasing expertise in project management and technical execution. Successfully led the complete lifecycle of the Read Aloud Challenge Portal, from concept to deployment. Implemented Vue.js functionality and design with precision, aligning closely with Figma specifications for a user-centric experience.',
+    stack: ['VueJS', 'Bootstrap', 'Pinia', 'SCSS'],
+    projects: [
+      {
+        title: 'Read Aloud Challenge Portal',
+        link: 'https://www.readaloudchallenge.co.uk/',
+      },
+    ]
+  },
+  {
+    title: 'Full Stack Developer',
+    company: 'Pictureworks Group Pty. Ltd.',
+    company_website: 'https://www.pictureworks.com.au/',
+    duration: {
+      from: 'March 2022',
+      to: 'Feb 2023'
     },
-    {
-        title: 'Full Stack Developer',
-        company: 'IFormatLogic IT Solutions',
-        company_website: 'https://iformatlogic.com/',
-        duration: {
-            from: 'June 2019',
-            to: 'Jan 2020'
-        },
-        description: `Designed and developed system features, demonstrating proficiency in technologies like Laravel, CakePHP, Symfony, VueJS, ReactJS, Shopify, and WordPress. Improved communication skills through regular interactions, fostering effective collaboration. Excelled in working under tight deadlines and thriving in a fast-paced environment. Managed and developed multiple projects, ensuring their continuous growth and evolution.`,
-        stack: ['Laravel', 'CakePHP', 'Wordpress', 'VueJS', 'ReactJS'],
-    },    
+    description: 'Joined a SaaS development team focused on managing printing businesses, contributing significantly to portal enhancement and maintenance. Utilized ReactJS for feature implementation and optimization of queries handling large datasets, enhancing platform performance. Proficiently translated design concepts into functional front-end code, demonstrating effective communication between design and execution in ReactJS.',
+    stack: ['ReactJS', 'Bootstrap', 'Laravel', 'PostgreSQL'],
+  },
+  {
+    title: 'Full Stack Developer',
+    company: 'Cargo Padala Express',
+    company_website: 'https://capex.com.ph/',
+    duration: {
+      from: 'Feb 2021',
+      to: 'March 2022'
+    },
+    description: 'Led the overhaul of Capex Booking Website from WordPress to Laravel, managing the project lifecycle. Implemented Laravel for backend, Laravel Blade for frontend, and MySql for efficient data handling. Integrated Google Maps API for enhanced package tracking. Also, spearheaded the development of a competitive TNVS application in the Philippines.',
+    stack: ['VueJS', 'Bootstrap', 'Laravel', 'MySQL'],
+    projects: [
+      {
+        title: 'Capex Booking',
+        link: 'https://app.capex.com.ph/',
+      },
+      {
+        title: 'BluePanther',
+        link: 'https://tnvs.capex.com.ph/'
+      }
+    ]
+  },
+  {
+    title: 'Full Stack Developer',
+    company: 'Tailin Abrasives Corporation',
+    company_website: 'https://www.tailinsubic.net/',
+    duration: {
+      from: 'Feb 2020',
+      to: 'August 2020'
+    },
+    description: `Joined Tailin as one of two inaugural developers, pivotal in forming the company's IT team. Played a key role in designing, developing, and maintaining the ERP system, overseeing functions like production management, reporting, attendance tracking, and internal communications. Specialized in backend development with NodeJS, successfully implementing advanced features, including real-time chat and live production tracking. Also contributed to translating design concepts into frontend code using Angular 8.`,
+    stack: ['Angular 8', 'MaterialCSS', 'Laravel', 'NodeJS'],
+  },
+  {
+    title: 'Full Stack Developer',
+    company: 'IFormatLogic IT Solutions',
+    company_website: 'https://iformatlogic.com/',
+    duration: {
+      from: 'June 2019',
+      to: 'Jan 2020'
+    },
+    description: `Designed and developed system features, demonstrating proficiency in technologies like Laravel, CakePHP, Symfony, VueJS, ReactJS, Shopify, and WordPress. Improved communication skills through regular interactions, fostering effective collaboration. Excelled in working under tight deadlines and thriving in a fast-paced environment. Managed and developed multiple projects, ensuring their continuous growth and evolution.`,
+    stack: ['Laravel', 'CakePHP', 'Wordpress', 'VueJS', 'ReactJS'],
+  },
 ])
